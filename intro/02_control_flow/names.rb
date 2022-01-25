98fb447f27bb510f3dea77744250a8b4ef73f185
Kurt Kerns <kurtkerns20@gmail.com>
2022-01-25 16:05:20 -0600

    solution for names.rb

--- intro/02_control_flow/names.rb
+++ intro/02_control_flow/names.rb
@@ -5,4 +5,17 @@ name3 = "Beric Dondarrion"
 name4 = "Melisandre"
 
 # Your code goes here
+avg_length = (name1.delete(' ').size + name2.delete(' ').size + 
+              name3.delete(' ').size + name4.delete(' ').size) / 4
 
+puts "=> What is your name?"
+print "=> "
+my_name = gets.chomp
+
+if my_name.delete(' ').size < avg_length
+  puts "#{my_name} is shorter than average"
+elsif my_name.delete(' ').size == avg_length
+  puts "#{my_name} is average in length"
+else
+  puts "#{my_name} is longer than average"
+end
