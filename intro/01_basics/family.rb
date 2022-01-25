515aa6dade4e6e775411300b16d7981530bc8b9a
Kurt Kerns <kurtkerns20@gmail.com>
2022-01-25 15:39:39 -0600

    solution for 01 intro

--- intro/01_basics/family.rb
+++ intro/01_basics/family.rb
@@ -5,4 +5,14 @@ john = 18
 mary = 16
 
 # Your code goes here
+age_sum = mom + dad + john + mary
 
+result = ((mom * dad) / (john - mary))
+
+puts "Sum of ages = #{age_sum}"
+puts "Result      = #{result}"
+
+# Extra Credit
+
+puts "#{age_sum} = Sum of ages"
+puts "#{result}  = Result"
